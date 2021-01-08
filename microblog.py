8804762496dd89395237f8b97a68dad36c09c898
dozzinator <laurens.verwaaijen@gmail.com>
2021-01-08 20:15:57 +0100

    heroku deployment changes

--- microblog.py
+++ microblog.py
@@ -3,8 +3,10 @@ from app.models import User, Post
 
 app = create_app()
 cli.register(app)
-
+ 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+## Hello
